3a97403f7d4042ef0b3a4670df07db5aa221f1e8
Dillorom <37942024+Dillorom@users.noreply.github.com>
2019-02-03 21:26:40 -0800

    create posts and list them from the database

---
+++ Blogging/Migrations/201902040323567_AddNewPost.cs
@@ -0,0 +1,16 @@
+namespace Blogging.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddNewPost : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
--- Blogging/Program.cs
+++ Blogging/Program.cs
@@ -10,33 +10,61 @@ namespace Blogging
     {
         static void Main(string[] args)
         {
+            //using (var db = new BloggingContext())
+            //{
+            //    // Create and save a new Blog
+            //    Console.Write("Enter a name for a new Blog: ");
+            //    var name = Console.ReadLine();
+
+            //    Console.WriteLine("Enter the URL: ");
+            //    string url = Console.ReadLine();
+
+            //    var blog = new Blog { Name = name, Url = url };
+            //    db.Blogs.Add(blog);
+            //    db.SaveChanges();
+
+            //    // Display all Blogs from the database
+            //    List<Blog> query = db.Blogs.OrderBy(d => d.Name).ToList();
+
+            //    Console.WriteLine("All blogs in the database:");
+            //    foreach (var item in query)
+            //    {
+            //        Console.WriteLine($"{item.Name} - {item.Url}");
+            //    }
+
+            //    Console.WriteLine("Press any key to exit...");
+            //    Console.ReadKey();
+            //}
+
             using (var db = new BloggingContext())
             {
-                // Create and save a new Blog
-                Console.Write("Enter a name for a new Blog: ");
-                var name = Console.ReadLine();
+                Console.WriteLine("Enter the post title: ");
+                string title = Console.ReadLine();
 
-                Console.WriteLine("Enter the URL: ");
-                string url = Console.ReadLine();
+                Console.WriteLine("Enter the post content: ");
+                string content = Console.ReadLine();
 
-                var blog = new Blog { Name = name, Url = url };
-                db.Blogs.Add(blog);
+                Console.WriteLine("Enter the blog id: ");
+                int blogId = int.Parse(Console.ReadLine());
+
+                var post = new Post { Title = title, Content = content, BlogId = blogId };
+                db.Posts.Add(post);
                 db.SaveChanges();
 
-                // Display all Blogs from the database
-                var query = from b in db.Blogs
-                            orderby b.Name
-                            select b;
+                var query = from p in db.Posts
+                            orderby p.Title
+                            select p;
 
                 Console.WriteLine("All blogs in the database:");
-                foreach (var item in query)
+                foreach (var p in db.Posts)
                 {
-                    Console.WriteLine($"{item.Name} - {item.Url}");
+                    Console.WriteLine($"{p.BlogId}. {p.Title} - {p.Content}");
                 }
 
                 Console.WriteLine("Press any key to exit...");
                 Console.ReadKey();
             }
+
         }
     }
 }
